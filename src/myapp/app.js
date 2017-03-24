127070d7eb1323922fa4dcf29f51c7949728f450
sr1994lu <sr1994lu@gmail.com>
2017-03-24 21:38:58 +0900

    build(core): Parse bodies in a middleware

--- src/myapp/app.js
+++ src/myapp/app.js
@@ -1,21 +1,27 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const multer = require('multer');
 const logger = require('morgan');
 const app = express();
 
 app.set('views', './views');
 app.set('view engine', 'ejs');
 
+// for parsing multipart/form-data
+const upload = multer();
+// for parsing application/json
+app.use(bodyParser.json());
+// for parsing application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true}));
+
 app.use(logger('dev'));
 app.use(express.static('public'));
 
-app.param('id', (req, res, next, id) => {
-  let users = ['taguchi', 'fkoji', 'dotinstall'];
-  req.params.name = users[id];
-  next();
+app.get('/new', (req, res) => {
+  res.render('new');
 });
-
-app.get('/hello/:id', (req, res) => {
-  res.send('hello ' + req.params.name);
+app.post('/create', (req, res) => {
+  res.send(req.body.name);
 });
 app.get('/bye/:id', (req, res) => {
   res.send('bye ' + req.params.name);
