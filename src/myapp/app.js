d429af4d7e525e1ef28ae7118395d09bd67414c6
Shunsuke NISHINO <sr1994lu@users.noreply.github.com>
2017-03-24 20:27:20 +0900

    build(myapp): use ejs (#5)

--- src/myapp/app.js
+++ src/myapp/app.js
@@ -8,8 +8,17 @@ app.set('view engine', 'ejs');
 app.use(logger('dev'));
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.render('index', {title: 'title'});
+app.param('id', (req, res, next, id) => {
+  let users = ['taguchi', 'fkoji', 'dotinstall'];
+  req.params.name = users[id];
+  next();
+});
+
+app.get('/hello/:id', (req, res) => {
+  res.send('hello ' + req.params.name);
+});
+app.get('/bye/:id', (req, res) => {
+  res.send('bye ' + req.params.name);
 });
 
 app.listen(3030, () => {
