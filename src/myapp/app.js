7e2edaa496a6e7e398d5b328f5f7e91d86dd4261
sr1994lu <sr1994lu@gmail.com>
2017-03-24 20:16:53 +0900

    build(myapp): use ejs

--- src/myapp/app.js
+++ src/myapp/app.js
@@ -2,17 +2,25 @@ const express = require('express');
 const logger = require('morgan');
 const app = express();
 
+app.set('views', './views');
+app.set('view engine', 'ejs');
+
 app.use(logger('dev'));
 app.use(express.static('public'));
-app.use((req, res, next) => {
-  console.log('my custom middleware!');
+
+app.param('id', (req, res, next, id) => {
+  let users = ['taguchi', 'fkoji', 'dotinstall'];
+  req.params.name = users[id];
   next();
 });
 
-app.get('/hello.txt', (req, res) => {
-  res.sendfile(require.resolve('./public/hello.txt'));
+app.get('/hello/:id', (req, res) => {
+  res.send('hello ' + req.params.name);
+});
+app.get('/bye/:id', (req, res) => {
+  res.send('bye ' + req.params.name);
 });
 
-app.listen(3000, () => {
+app.listen(3030, () => {
   console.log('Server Starting...');
 });
